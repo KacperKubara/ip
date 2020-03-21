3666a8f719d2e44b11bf6d2701bfbc9c6af0f3c4
KacperKubara <kjk1u17@soton.ac.uk>
2020-03-21 19:29:58 +0000

    Batch Update

--- benchmark1_2.py
+++ benchmark1_2.py
@@ -1,6 +1,7 @@
 # Script implementing the 1st benchmark from my IP project
 import json
 import logging
+from copy import deepcopy
 logging.getLogger().setLevel(logging.INFO)
 
 import numpy as np
@@ -37,9 +38,10 @@ params_dict = {
         "activation_fns": [tf.nn.leaky_relu]
     }
 }
+params_save = {}
 splitter_methods = ["random"]
 path_results = "./results/benchmark1_2/results_benchmark1_2.json"
-
+path_best_params = "./results/benchmark1_2/results_benchmark1_2_params.json"
 
 if __name__ == "__main__":
     results = {}
@@ -80,14 +82,16 @@ if __name__ == "__main__":
             # For smaller size scaffolds use rms
             metric = dc.metrics.Metric(dc.metrics.mae_score)
             opt = HyperparamOpt(model_builder)
-            params_best, score_best = opt.hyperparam_search(params_model, 
-                                                            wang_train,
-                                                            wang_valid,
-                                                            wang_transformers,
-                                                            metric)
-
+            params_best, score_best, all_results = opt.hyperparam_search(params_model, 
+                                                                        wang_train,
+                                                                        wang_valid,
+                                                                        wang_transformers,
+                                                                        metric)
+            
             logging.info(f"Best score for {model_name}: {score_best}")
+            logging.info(f"All results for {model_name}: {all_results}")
             logging.info(f"Best params for {model_name}: {params_best}")
+            params_save[model_name] = deepcopy(params_best)
             model = model_obj(**params_best)
             
             logging.info(f"Fitting the best model model: {model_name}")
@@ -106,7 +110,9 @@ if __name__ == "__main__":
             results[splitter][model_name]["train_score"] = train_scores
             results[splitter][model_name]["valid_score"] = valid_scores
 
-    logging.info(results)
-    logging.info(f"Results has been saved to {path_results}")
-    with open(path_results, 'w') as json_f:
-        json.dump(results, json_f)
+        logging.info(results)
+        logging.info(f"Results has been saved to {path_results}")
+        with open(path_results, 'w') as json_f:
+            json.dump(results, json_f)
+        with open(path_best_params, 'w') as json_f:
+            json.dump(params_best, json_f)
